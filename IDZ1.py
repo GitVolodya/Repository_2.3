9b50d9f290741c3f26e19b22cd12b7b3bb58cba4
Volodya <vova.x23@mail.ru>
2023-06-08 12:16:20 +0300

    IDZ1

---
+++ IDZ1.py
@@ -0,0 +1,7 @@
+text = "Это текст со знаками плюса + и минуса -"
+
+count_plus = text.count('+')
+count_minus = text.count('-')
+
+print(f"Количество символов «+» в тексте: {count_plus}")
+print(f"Количество символов «-» в тексте: {count_minus}")
